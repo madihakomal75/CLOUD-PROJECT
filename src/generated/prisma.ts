4674b915fce4eb08c534a97082745a75a3b76d13
madihakomal75 <madihakomal75@gmail.com>
2026-05-11 19:54:30 +0500

    Fix prisma output path to src/generated/prisma

---
+++ src/generated/prisma.ts
@@ -0,0 +1,12 @@
+// This file re-exports Prisma generated types for convenient importing
+export type {
+  NodeType,
+  CredentialType,
+  Credential,
+  User,
+  Workflow,
+  WorkflowExecution,
+  WorkflowStep,
+} from "@prisma/client";
+
+export { NodeType, CredentialType } from "@prisma/client";
